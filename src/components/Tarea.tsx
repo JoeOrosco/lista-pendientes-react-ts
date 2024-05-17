7b2eb9257fae7df445db605ed10aca9c167301ab
Jaime Orosco <jaimeorosco501@gmail.com>
2024-05-16 22:11:34 -0500

    Proyecto final de lista de tareas con React y Typescript

---
+++ src/components/Tarea.tsx
@@ -0,0 +1,14 @@
+
+interface Tarea {
+  tarea: string
+  borrarTarea: () => void
+}
+
+export const Tarea = ({ tarea, borrarTarea }: Tarea) => {
+  return (
+    <div className="task">
+      <span>{tarea}</span>
+      <button onClick={borrarTarea}>Borrar</button>
+    </div>
+  )
+}
